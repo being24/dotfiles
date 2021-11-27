8109bc95fdd58eacfa5ef00c1dba5969f91f15f0
being <34680324+being24@users.noreply.github.com>
2021-11-27 15:35:57 +0900

    🎨

--- ubuntu_initial_setting.sh
+++ ubuntu_initial_setting.sh
@@ -6,7 +6,7 @@ sudo sed -i.bak -e "s%http://jp.archive.ubuntu.com/ubuntu/%http://ftp.jaist.ac.j
 sudo apt-get update && sudo apt-get upgrade -y
 
 # gitのインストール
-sudo apt-get install git xclip tmux -y
+sudo apt-get install git xclip tmux curl -y
 
 # git にユーザー名とemailを追加
 git config --global user.name "being"
@@ -26,22 +26,17 @@ sudo apt-get install bat -y
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
-#fdのインストール
-sudo apt-get install fd-find -y
-
-#exaのインストール
-wget http://archive.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
-sudo apt-get install ./exa_0.9.0-4_amd64.deb
-rm ./exa_0.9.0-4_amd64.deb
-
 # bash it 入れる
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 
+# alacrittyのインストール
+# 設定のclone + シンボリックリンク
+# PS1の設定を見直す
+
+# alacritty-colorscheme -V apply solarized_dark.yamlをできるようにする
+
 bash-it enable completion docker docker-compose docker-machine git pip3 ssh tmux
 
 echo alias cat='batcat' >> .bashrc
-echo alias fd='fdfind' >> .bashrc
-echo alias ls='exa' >> .bashrc
-sed -i -e 's/bobby/pure/g' ~/.bashrc
 source ~/.bashrc
