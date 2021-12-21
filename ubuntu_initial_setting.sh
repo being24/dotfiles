0f308ef2153f53336e50b5aad72677b0618bfbc4
being <34680324+being24@users.noreply.github.com>
2021-12-21 17:19:32 +0900

    🎨

--- ubuntu_initial_setting.sh
+++ ubuntu_initial_setting.sh
@@ -6,13 +6,19 @@ sudo sed -i.bak -e "s%http://jp.archive.ubuntu.com/ubuntu/%http://ftp.jaist.ac.j
 sudo apt-get update && sudo apt-get upgrade -y
 
 # gitのインストール
-sudo apt-get install git xclip tmux curl -y
+sudo apt-get install git xclip curl fcitx-mozc -y
 
 # git にユーザー名とemailを追加
 git config --global user.name "being"
 git config --global user.email "34680324+being24@users.noreply.github.com"
 
-sudo apt-get install python-pip python2.7 python2.7-dev python3
+mkdir programs
+cd pregrams
+
+git clone https://github.com/being24/dotfiles.git
+cd ~
+
+sudo apt-get install python3-pip python3
 
 # microのインストール
 curl https://getmic.ro | bash
@@ -24,13 +30,62 @@ sudo cp .bashrc .bashrc.origin
 #batのインストール
 sudo apt-get install bat -y
 
-# alacrittyのインストール
-# 設定のclone + シンボリックリンク
-# PS1の設定を見直す
-
-# alacritty-colorscheme -V apply solarized_dark.yamlをできるようにする
-
-# PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+sudo apt autoremove -y
+sudo apt autoclean -y
 
 echo alias cat='batcat' >> .bashrc
-source ~/.bashrc
+
+# cica フォントのインストール
+mkdir cica
+cd cica
+curl -L -O https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_with_emoji.zip
+unzip Cica_v5.0.2_with_emoji.zip
+sudo mkdir  /usr/share/fonts/truetype/cica
+sudo cp *.ttf /usr/share/fonts/truetype/cica/
+sudo fc-cache -vf
+cd ~
+rm -rf cica
+
+# alacrittyのインストール
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+. $HOME/.cargo/env
+cd programs
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+rustup override set stable
+rustup update stable
+cargo build --release
+
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+
+echo "source $(pwd)/extra/completions/alacritty.bash" >> ~/.bashrc
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
+
+mkdir $HOME/.config/alacritty/
+sudo ln -s ~/programs/dotfiles/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+
+cd ~
+
+mkdir ~/programs/zellij
+cd ~/programs/zellij
+curl -s https://api.github.com/repos/zellij-org/zellij/releases/latest | grep zellij-x86_64 | grep linux | grep browser_download_url | cut -d : -f 2,3 | tr -d \" | wget -qi -
+tar -xzvf zellij-x86_64-unknown-linux-musl.tar.gz
+rm zellij-x86_64-unknown-linux-musl.tar.gz
+sudo mv zellij /usr/local/bin
+chmod +x /usr/local/bin/zellij
+
+echo "PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+echo '[ -z "$ZELLIJ" ] && zellij' >> ~/.bashrc
+
+. ~/.bashrc
+
+# code chrome
